c1f447f816e8aaa829c7a173bc9dcf465dd05586
shawnmmartin <40648168+shawnmmartin@users.noreply.github.com>
2025-01-08 09:55:56 -0500

    fix alb errors

--- main.tf
+++ main.tf
@@ -56,21 +56,20 @@ module "alb" {
   security_groups = [module.blog_sg.security_group_id]
 
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
-  }
+  ]
 
   http_tcp_listeners = [
     {
-      port                = 80
-      protocol            = "HTTP"
-      target_group_index  = 0
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
   ]
 
