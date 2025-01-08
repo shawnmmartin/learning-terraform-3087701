bdb1959b1913a9a69defa4dbfd5fd82cef8eb9c2
shawnmmartin <40648168+shawnmmartin@users.noreply.github.com>
2025-01-08 09:51:41 -0500

    load balancer

--- main.tf
+++ main.tf
@@ -47,6 +47,38 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_sg.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  http_tcp_listeners = [
+    {
+      port                = 80
+      protocol            = "HTTP"
+      target_group_index  = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Devedevlopment"
+  }
+}
+
 
 
 module "blog_sg" {
